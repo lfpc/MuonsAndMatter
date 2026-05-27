9bab0566ed6fb89deb68f39094b7a3b808576afb
lfpc <luisfelipe1998@gmail.com>
2026-05-27 13:03:01 +0200

    small details in env setting

--- shell_container.sh
+++ shell_container.sh
@@ -1,5 +1,5 @@
 export APPTAINER_CMD=/disk/users/`whoami`/apptainer/bin/apptainer
 export PROJECTS_DIR="$(dirname "$PWD")"
-export SAVEDIR=/scratch/lprate
+export SAVEDIR=/scratch/`whoami`
 apptainer exec --nv -B /cvmfs -B /scratch/`whoami` -B /disk/users/`whoami` -B /home/hep/`whoami` \
             /disk/users/lprate/containers/MuonsAndMatterContainer.sif bash --rcfile set_env.sh
